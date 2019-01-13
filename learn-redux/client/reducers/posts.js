51940faa4558768591b46df46a55257b0ee1e7f3
jonathanhoy <jonathanhoy5@gmail.com>
2019-01-13 13:48:25 -0500

    lesson 13

--- learn-redux/client/reducers/posts.js
+++ learn-redux/client/reducers/posts.js
@@ -3,8 +3,20 @@
 // 2. copy of current state
 
 function posts(state = [], action) {
-  console.log('the post will change');
-  console.log(state, action);
+  switch(action.type) {
+    case 'INCREMENT_LIKES' :
+      console.log('incrementing likes');
+      const i = action.index
+      return [
+        ...state.slice(0, i), // before the one we are updating
+        {...state[i], likes: state[i].likes + 1},
+        ...state.slice(i + 1) // after the one we are updating
+      ]
+      
+    // return the updated state
+    default:
+      return state;
+  }
   return state;
 }
 
